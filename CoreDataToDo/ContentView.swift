f3462e79bf882a4dfa719012f9285e2e86909605
Philip Smith <techbyphil@gmail.com>
2019-09-07 12:28:25 -0700

    Initial Commit

---
+++ CoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+//
+//  Created by Philip Smith on 9/7/19.
+//  Copyright © 2019 Philip Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
